e8d83e6ff84f9488294e227c88185b6790394732
Leg3ndre <leg3ndre@gmail.com>
2024-04-07 00:14:11 +0900

    const分離

---
+++ lib/const.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require 'dotenv/load'
+
+DICT_PATH = ENV.fetch('MECAB_DICTIONARY_PATH')
--- main.rb
+++ main.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 require 'natto'
-require 'dotenv/load'
-
-DICT_PATH = ENV.fetch('MECAB_DICTIONARY_PATH')
+require './lib/const'
 
 option = DICT_PATH ? "-d #{DICT_PATH}" : nil
 nm = Natto::MeCab.new(option)
-puts nm
 
 puts nm.parse('8月3日に放送された「中居正広の金曜日のスマイルたちへ」(TBS系)で、1日たった5分でぽっこりおなかを解消するというダイエット方法を紹介。キンタロー。のダイエットにも密着。')
