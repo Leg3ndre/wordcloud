05570ba0ce1ef45f41ffd2584dbfbf1e24ad0da0
Leg3ndre <leg3ndre@gmail.com>
2024-04-07 04:27:51 +0900

    サニタイズする

--- lib/source.rb
+++ lib/source.rb
@@ -9,4 +9,14 @@ class Source
   def initialize
     @content = File.read(SOURCE_PATH)
   end
+
+  private
+
+  def sanitize
+    @content.gsub!(/(ほ|お)ォ/) { "#{$1}お" }
+    @content.gsub!(/(あ|か|さ|だ|な|ま|や|～)ぁ/) { "#{$1}あ" }
+    @content.gsub!(/[！？]+/, '。')
+    @content.gsub!(/\.{3}/, '')
+    @content.tr!('～', 'ー')
+  end
 end
